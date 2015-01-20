003e42b0334f4c2d0ced8007cf3098bb2b991d29
MaximeHelaine <mhelainedu91@live.fr>
2015-01-20 23:02:41 +0100

    Passage du model du flipper en .fbx et création d'un Ball.physique
    Me dire si ça fonctionne

--- PongKinect/Assets/Scripts/Actions/MouvementBall.cs
+++ PongKinect/Assets/Scripts/Actions/MouvementBall.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class MouvementBall : MonoBehaviour {
     [SerializeField]
     private float MinVelocity = 1.0f;
+
+    [SerializeField]
+    private float StartImpulsion = 10.0f;
 	// Use this for initialization
 	void Start () {
-        rigidbody.AddForce(Vector3.forward , ForceMode.Acceleration);
+        rigidbody.AddForce(Vector3.forward * StartImpulsion, ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        ClampMagnitude();
+        //ClampMagnitude();
 	}
+    //Permet de garder une vitesse minimum pour la balle
     void ClampMagnitude()
     {
         Vector3 vel = rigidbody.velocity;
